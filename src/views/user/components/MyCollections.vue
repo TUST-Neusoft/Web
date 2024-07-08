8596690d06fd527a2f53db726673279d4194b23e
dogmao <124482109+dogmao@users.noreply.github.com>
2024-07-08 10:19:31 +0800

    Add files via upload

---
+++ src/views/user/components/MyCollections.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <!-- 我的收藏内容 -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        collections: []
+      };
+    },
+    methods: {
+      fetchCollections() {
+        // 获取收藏数据逻辑
+      }
+    },
+    created() {
+      this.fetchCollections();
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* 样式 */
+  </style>
+  
